a2370b1fab45fe27576becf16108eb7ea3143d35
DerGoogler <support@dergoogler.com>
2022-06-02 21:16:39 +0200

    Remove ANSI ui_prints

--- customize.sh
+++ customize.sh
@@ -20,21 +20,33 @@ systemWrite() {
     fi
 }
 
-if [ ! "$ANSI_SUPPORT" == "true" ] || [ ! -n "$MMM_EXT_SUPPORT" ]; then
-    abort "! This module need to be executed in Fox's Magisk Module Manager with ANSI support"
+#if [ ! "$ANSI_SUPPORT" == "true" ] || [ ! -n "$MMM_EXT_SUPPORT" ]; then
+#    abort "! This module need to be executed in Fox's Magisk Module Manager with ANSI support"
+#    exit 1
+#fi
+
+if [ -n "$MMM_EXT_SUPPORT" ]; then
+    ui_print "#!useExt"
+    mmm_exec() {
+        ui_print "$(echo "#!$@")"
+    }
+else
+    mmm_exec() { true; }
+    abort "! Dieses Modul muss im Fox's Magisk Module Manager ausgefuert werden"
     exit 1
 fi
 
-for i in {16..21} {21..16} ; do echo -en "\e[38;5;${i}m#\e[0m" ; done ; echo
+#for i in {16..21} {21..16} ; do ui_print "\e[38;5;${i}m#\e[0m" ; done ; ui_print
 systemWrite true
 
 arr=( "transfer" "zip" "bash" "wget" "aapt" "pip" "python" "node" "yarn" "systemWrite" ""  )
 
 for item in "${arr[@]}"
 do
-    ui_print "\e[38;5;82mMaking \e[34m${item} \e[38;5;198mExecuteable"
+    #ui_print "\e[38;5;82mMaking \e[34m${item} \e[38;5;198mExecuteable"
+    ui_print "Making ${item} Executeable"
     chmodBin $item
 done
 
 systemWrite false
-for i in {16..21} {21..16} ; do echo -en "\e[38;5;${i}m#\e[0m" ; done ; echo
+#for i in {16..21} {21..16} ; do ui_print -en "\e[38;5;${i}m#\e[0m" ; done ; ui_print
