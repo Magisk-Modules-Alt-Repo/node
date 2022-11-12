ed2716e8a7e4e33cbd7f733acfa5905b58498567
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2022-11-12 15:32:30 +0100

    Softlink /data/local/.config to /.config

--- service.sh
+++ service.sh
@@ -5,6 +5,9 @@ MODDIR=${0%/*}
 
 # This is required because "#!/usr/bin/env node" cannot loaded.
 ln -s -T /system /usr
+# Softlink /data/local/.config to /.config to avoid install or configuration issues
+ln -s -T /data/local/.config /.config
+
 
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
