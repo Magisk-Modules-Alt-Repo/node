598a1ddbd20f7d7b8d22f2e5749f8b199a12b5b9
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2022-11-19 17:41:40 +0100

    Wait for finished boot

--- service.sh
+++ service.sh
@@ -3,6 +3,16 @@
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
+# Wait for boot to finish completely
+dbg "Sleeping until boot completes."
+while [[ `getprop sys.boot_completed` -ne 1 ]]
+do
+  sleep 1
+done
+
+# Sleep an additional 20s to ensure init is finished
+sleep 20
+
 # This is required because "#!/usr/bin/env node" cannot loaded.
 ln -s -T /system /usr
 
