589612ab3e4e169ced628096efbfb775211a9446
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2022-12-19 16:02:19 +0100

    Create service.sh

---
+++ service.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+
+notify() {
+  su -lp 2000 -c "cmd notification post -S bigtext -t '$1' '$RANDOM' '$2'"
+}
+
+TITLE="Node.js Service Startup"
+SPATH="/sdcard/Documents/Node"
+
+while [[ `getprop sys.boot_completed` -ne 1 ]]
+do
+  sleep 1
+done
+
+sleep 120
+
+if ! command -v nohup > /dev/null;then
+    notify "$TITLE" "The \"nohup\" binary was not found! Please ensure you have it installed."
+    exit 1
+fi
+
+if [ ! -d "$SPATH" ]; then
+  mkdir $SPATH
+  echo "console.log(\"Log!\")" >$SPATH/log.js
+fi
+
+if [ -d "$SPATH" ]; then
+  FILE_COUNT=$(ls $SPATH/*.js | wc -l)
+  for script in $SPATH/*.js; do
+    if [ -f $script ]; then
+      nohup node $script >/dev/null 2>&1 &
+    fi
+  done
+  notify "$TITLE" "$FILE_COUNT file/s has been detected, unknown if all has been executed."
+fi
+
+unset FILE_COUNT script SPATH TITLE notify
