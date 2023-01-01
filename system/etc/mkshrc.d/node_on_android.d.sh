cdcd4a4c75ce3d673310383a4857c39ac47d00ed
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2023-01-01 15:52:38 +0100

    env changes

--- system/etc/mkshrc.d/node_on_android.d.sh
+++ system/etc/mkshrc.d/node_on_android.d.sh
@@ -3,16 +3,16 @@ mkshrc-add-path /system/usr/share/.yarn/bin \
        $HOME/.config/yarn/global/node_modules/.bin
 
 # Symlink /system into /usr. This is only a requirement for the Node.js module, to make binaries work.
-sudo mount -o rw,remount /
+sudo mount -o rw,remount / >/dev/null
 if [ ! -d "/usr" ]; then
-  sudo ln -s -T /system /usr
+  sudo ln -s -T /system /usr >/dev/null
 fi
-sudo mount -o ro,remount /
+sudo mount -o ro,remount / >/dev/null
 
 # Ability for npm packages to inject into the environment
 # NPM requires installed (yarn global add npm)
 for rc in $PREFIX/lib/node_modules/*/mkshrc.sh; do
   if [ -f $rc ]; then
-    . $rc
+    source $rc
   fi
 done
