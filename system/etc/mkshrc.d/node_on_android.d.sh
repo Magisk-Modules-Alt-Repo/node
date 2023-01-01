2d7a44f98a6d74800c0be5c0376a047af3f210ff
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2023-01-01 03:06:32 +0100

    Update node_on_android.d.sh

--- system/etc/mkshrc.d/node_on_android.d.sh
+++ system/etc/mkshrc.d/node_on_android.d.sh
@@ -1,5 +1,6 @@
 ## Node.js module requirements
-export PATH="$PATH:/system/usr/share/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"
+mkshrc-add-path /system/usr/share/.yarn/bin \
+       $HOME/.config/yarn/global/node_modules/.bin
 
 # Symlink /system into /usr. This is only a requirement for the Node.js module, to make binaries work.
 sudo mount -o rw,remount /
