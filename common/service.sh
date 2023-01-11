62b644abcc47cc1539eff1a8ffa67d6c0189313e
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2023-01-11 18:46:10 +0100

    Added loggin to service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,5 +1,17 @@
 #!/system/bin/sh
 
+logi() {
+  if command -v log >/dev/null; then
+    log -p i "$1" -t "nodejs"
+  fi
+}
+
+loge() {
+  if command -v log >/dev/null; then
+    log -p e "$1" -t "nodejs"
+  fi
+}
+
 notify() {
   su -lp 2000 -c "cmd notification post -S bigtext -t '$1' '$RANDOM' '$2'"
 }
@@ -7,21 +19,22 @@ notify() {
 TITLE="Node.js Service Startup"
 SPATH="/data/chuser/root/usr/etc/node.d"
 
-while [[ `getprop sys.boot_completed` -ne 1 ]]
-do
+while [[ $(getprop sys.boot_completed) -ne 1 ]]; do
   sleep 1
 done
 
 sleep 120
 
-if ! command -v nohup > /dev/null;then
-    notify "$TITLE" "The \"nohup\" binary was not found! Please ensure you have it installed."
-    exit 1
+if ! command -v nohup >/dev/null; then
+  notify "$TITLE" "The \"nohup\" binary was not found! Please ensure you have it installed."
+  loge "nohup binary wasn't found"
+  exit 1
 fi
 
 if [ ! -d "$SPATH" ]; then
   mkdir $SPATH
   echo "console.log(\"Log!\")" >$SPATH/log.js
+  logi "log.js file has been successfully created"
 fi
 
 if [ -d "$SPATH" ]; then
@@ -29,9 +42,11 @@ if [ -d "$SPATH" ]; then
   for script in $SPATH/*.js; do
     if [ -f $script ]; then
       nohup node $script >/dev/null 2>&1 &
+      logi "$script has been executed with \"nohup\""
     fi
   done
   notify "$TITLE" "$FILE_COUNT file/s has been detected, unknown if all has been executed."
+  logi "$FILE_COUNT file/s has been detected"
 fi
 
-unset FILE_COUNT script SPATH TITLE notify
+unset FILE_COUNT script SPATH TITLE notify logi loge
