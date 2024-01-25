03da54ab426b412083429e58d7e28caeb90eb536
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-01-25 10:13:03 +0100

    1.5.5

--- system/usr/share/mmrl/config/node_on_android/index.jsx
+++ system/usr/share/mmrl/config/node_on_android/index.jsx
@@ -1,41 +1,83 @@
 import React from "react";
-import { Page } from "@mmrl/ui";
-import { useNativeProperties } from "@mmrl/hooks";
+import { Page, Toolbar } from "@mmrl/ui";
+import {
+  useNativeProperties,
+  useActivity,
+  useStrings
+} from "@mmrl/hooks";
+import { StringsProvider } from "@mmrl/providers";
 import { BugReport } from "@mui/icons-material";
 import { ListItem, ListItemIcon } from "@mui/material";
+
+const strs = {
+  en: {
+    title: "Configure Node.js",
+    service: "Service",
+    report_issue: "Report a issue",
+    project: "Project",
+    logging: "Logging",
+    s_up_notify: "Start up notification",
+    desc_notice: "Description notice",
+    desc_notice_sub: "Shows a text in the module description and shows how many service scripts are running"
+  },
+  de: {
+    report_issue: "Ein Problem melden",
+    project: "Projekt",
+    logging: "Protokollierung",
+    s_up_notify: "Startbenachrichtigung",
+    desc_notice: "Beschreibungshinweis",
+    desc_notice_sub: "Zeigt einen Text in der Modulbeschreibung an und zeigt an, wie viele Service-Skripte ausgeführt werden"
+  },
+}
 
 function NodeConfig() {
   const [notify, setNotify] = useNativeProperties("persist.nodejs.notify", true);
   const [logging, setLogging] = useNativeProperties("persist.nodejs.logging", false);
   const [desc, setDesc] = useNativeProperties("persist.nodejs.desc", true);
 
+  const { context } = useActivity()
+  const { strings } = useStrings()
+
+  const renderToolbar = () => {
+    return (
+      <Toolbar modifier="noshadow">
+        <Toolbar.Left>
+          <Toolbar.BackButton onClick={context.popPage} />
+        </Toolbar.Left>
+        <Toolbar.Center>
+          {strings("title")}
+        </Toolbar.Center>
+      </Toolbar>
+    )
+  }
+
   return (
-    <Page sx={{ p: 0 }}>
-      <List subheader={<ListSubheader>Service</ListSubheader>}>
+    <Page sx={{ p: 0 }} renderToolbar={renderToolbar}>
+      <List subheader={<ListSubheader>{strings("service")}</ListSubheader>}>
         <ListItem>
-          <ListItemText primary="Startup notification" />
+          <ListItemText primary={strings("s_up_notify")} />
           <Switch checked={notify} onChange={(e) => setNotify(e.target.checked)} />
         </ListItem>
         <ListItem>
-          <ListItemText primary="Logging" />
+          <ListItemText primary={strings("logging")} />
           <Switch checked={logging} onChange={(e) => setLogging(e.target.checked)} />
         </ListItem>
         <ListItem>
-          <ListItemText primary="Description notice" secondary="Show a text in the description how many service scripts are in the selected folder" />
+          <ListItemText primary={strings("desc_notice")} secondary={strings("desc_notice_sub")} />
           <Switch checked={desc} onChange={(e) => setDesc(e.target.checked)} />
         </ListItem>
       </List>
 
       <Divider />
 
-      <List subheader={<ListSubheader>Project</ListSubheader>}>
+      <List subheader={<ListSubheader>{strings("project")}</ListSubheader>}>
 
         <ListItem disablePadding>
           <ListItemButton onClick={() => window.open("https://github.com/Magisk-Modules-Alt-Repo/node/issues")}>
             <ListItemIcon>
               <BugReport />
             </ListItemIcon>
-            <ListItemText primary="Report a issue" />
+            <ListItemText primary={strings("report_issue")} />
           </ListItemButton>
         </ListItem>
       </List>
@@ -43,4 +85,12 @@ function NodeConfig() {
   );
 }
 
-export default NodeConfig;
+const NodeConfigMain = () => {
+  return (
+    <StringsProvider data={strs}>
+      <NodeConfig />
+    </StringsProvider>
+  )
+}
+
+export default NodeConfigMain;
